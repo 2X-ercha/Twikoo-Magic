7d6315da2b3cfd827542473893da1aac3ed1cd14
noionion <noionion@outlook.com>
2021-02-18 17:19:09 +0800

    新增滑稽

--- py/beautiful.py
+++
@@ -1,6 +0,0 @@
-import json
-
-with open("./py/twikoo/weisuomeng.json", "r", encoding="utf-8") as f:
-    j = json.load(f)
-with open("./py/twikoo/weisuomeng.json", "w") as f:
-    f.write(json.dumps(j, indent=2, separators=(',', ':')))
--- py/unbeautiful.py
+++
@@ -1,6 +0,0 @@
-import json
-
-with open("owo_beau.json", "r", encoding="utf-8") as owo_beau:
-    owo_beau = json.load(owo_beau)
-with open("owo.json", "w", encoding="utf-8") as owo:
-    owo.write(json.dumps(owo_beau, ensure_ascii=False))
